72e0482bbaafae9044af6d4cae4cc683082b318c
dirabbieto <dirabbieto@gmail.com>
2020-08-28 10:50:51 +0700

    updated data

---
+++ main.js
@@ -0,0 +1,160 @@
+function checksoal0(){
+
+    var soal1 = document.getElementById("textbox").value
+
+        if (soal1 == "rano karno") {
+            document.getElementById("jawaban").style.visibility = "visible"
+            document.getElementById("jawaban").innerText = "jokes 90an, ga ngerti kebangetan bre"
+            document.getElementById("jawabanBenar").style.visibility = "visible"
+
+        } else {
+            document.getElementById("jawaban").style.visibility = "visible"
+            document.getElementById("jawaban").innerText = 'noooope, anda kurang beruntung, silahkan coba lagi'        }
+
+
+
+}
+
+function checksoal1(){
+
+    var soal1 = document.getElementById("textbox").value
+
+        if (soal1 == "sunat") {
+            document.getElementById("jawaban").style.visibility = "visible"
+            document.getElementById("jawaban").innerText = "hmm beginner's luck"
+            document.getElementById("jawabanBenar").style.visibility = "visible"
+
+        } else {
+            document.getElementById("jawaban").style.visibility = "visible"
+            document.getElementById("jawaban").innerText = 'noooope, anda kurang beruntung, silahkan coba lagi'        }
+
+
+
+}
+
+function checksoal2(){
+
+    var soal1 = document.getElementById("textbox").value
+
+        if (soal1 == "konde") {
+            document.getElementById("jawaban").style.visibility = "visible"
+            document.getElementById("jawaban").innerText = "ngana bole juga"
+            document.getElementById("jawabanBenar").style.visibility = "visible"
+
+        } else {
+            document.getElementById("jawaban").style.visibility = "visible"
+            document.getElementById("jawaban").innerText = 'noooope, anda kurang beruntung, silahkan coba lagi'        }
+
+
+
+}
+
+function checksoal3(){
+
+    var soal1 = document.getElementById("textbox").value
+
+        if (soal1 == "gosip") {
+            document.getElementById("jawaban").style.visibility = "visible"
+            document.getElementById("jawaban").innerText = "udah ya pemanasannya, abis ini serius"
+            document.getElementById("jawabanBenar").style.visibility = "visible"
+
+        } else {
+            document.getElementById("jawaban").style.visibility = "visible"
+            document.getElementById("jawaban").innerText = 'noooope, anda kurang beruntung, silahkan coba lagi'        }
+
+
+
+}
+
+
+function checksoal4(){
+
+    var soal1 = document.getElementById("textbox").value
+
+        if (soal1 == "gosong") {
+            document.getElementById("jawaban").style.visibility = "visible"
+            document.getElementById("jawaban").innerText = "ntap"
+            document.getElementById("jawabanBenar").style.visibility = "visible"
+
+        } else {
+            document.getElementById("jawaban").style.visibility = "visible"
+            document.getElementById("jawaban").innerText = 'noooope, anda kurang beruntung, silahkan coba lagi'        }
+
+}
+
+function checksoal5(){
+
+    var soal1 = document.getElementById("textbox").value
+
+        if (soal1 == "jambret") {
+            document.getElementById("jawaban").style.visibility = "visible"
+            document.getElementById("jawaban").innerText = "masi gampang elah"
+            document.getElementById("jawabanBenar").style.visibility = "visible"
+
+        } else {
+            document.getElementById("jawaban").style.visibility = "visible"
+            document.getElementById("jawaban").innerText = 'noooope, anda kurang beruntung, silahkan coba lagi'        }
+
+}
+
+function checksoal6(){
+
+    var soal1 = document.getElementById("textbox").value
+
+        if (soal1 == "menteng") {
+            document.getElementById("jawaban").style.visibility = "visible"
+            document.getElementById("jawaban").innerText = "masi gampang elah"
+            document.getElementById("jawabanBenar").style.visibility = "visible"
+
+        } else {
+            document.getElementById("jawaban").style.visibility = "visible"
+            document.getElementById("jawaban").innerText = 'noooope, anda kurang beruntung, silahkan coba lagi'        }
+
+}
+
+function checksoal7(){
+
+    var soal1 = document.getElementById("textbox").value
+
+        if (soal1 == "sinting") {
+            document.getElementById("jawaban").style.visibility = "visible"
+            document.getElementById("jawaban").innerText = "masi betah aja main beginian buset sampe level 7"
+            document.getElementById("jawabanBenar").style.visibility = "visible"
+
+        } else {
+            document.getElementById("jawaban").style.visibility = "visible"
+            document.getElementById("jawaban").innerText = 'noooope, anda kurang beruntung, silahkan coba lagi'        }
+
+}
+
+function checksoal8(){
+
+    var soal1 = document.getElementById("textbox").value
+
+        if (soal1 == "goyang dombred") {
+            document.getElementById("jawaban").style.visibility = "visible"
+            document.getElementById("jawaban").innerText = "salah deng, sebelumnnya level 8 euy, maap"
+            document.getElementById("jawabanBenar").style.visibility = "visible"
+
+        } else {
+            document.getElementById("jawaban").style.visibility = "visible"
+            document.getElementById("jawaban").innerText = 'noooope, anda kurang beruntung, silahkan coba lagi'        }
+
+}
+
+function checksoal9(){
+
+    var soal1 = document.getElementById("textbox").value
+
+        if (soal1 == "air comberan") {
+            document.getElementById("jawaban").style.visibility = "visible"
+            document.getElementById("jawaban").innerText = "ente nganggur apa gimane, betah main beginian sampe level ini"
+            document.getElementById("jawabanBenar").style.visibility = "visible"
+
+        } else {
+            document.getElementById("jawaban").style.visibility = "visible"
+            document.getElementById("jawaban").innerText = 'noooope, anda kurang beruntung, silahkan coba lagi'        }
+
+}
+
+
